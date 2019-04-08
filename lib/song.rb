7c4976ed39120ac97bb6c037d62e7a7f14116deb
William Sinn <w.h.sinn@gmail.com>
2019-04-08 00:28:03 -0400

    first-commit

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,14 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+
+    @@all << self
+  end
   def self.all
     @@all
   end
@@ -10,4 +17,18 @@ class Song
     self.class.all << self
   end
 
+  def self.create(name)
+    name = self.new
+    self.name = name
+    name.save
+    self.all.include?(name)
+  end
+
+  def self.new_by_name(song)
+
+  end
 end
+
+hello = Song.create("hello")
+
+by_name = Song.new_by_name("By Name")
